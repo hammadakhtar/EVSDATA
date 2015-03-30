1e852fcee4eb352486b041ee7e5651af91ddc842
hammadakhtar <hammadakhtar078@gmail.com>
2015-03-31 02:18:17 +0530

    Classes created

---
+++ src/AirQualityIndex.java
@@ -0,0 +1,5 @@
+/**
+ * Created by lenovo on 3/31/2015.
+ */
+public class AirQualityIndex {
+}
---
+++ src/Client.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 3/31/2015.
+ */
+public class Client {
+    public static void main(String[] args)
+    {
+
+        System.out.println("hello world");
+    }
+}
