2cb1598dde749b5378be84ccb18c80da2633d2b5
hammadakhtar <hammadakhtar078@gmail.com>
2015-03-31 03:15:05 +0530

    Constructor and Data Structure

---
+++ src/DataFormatException.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lenovo on 3/31/2015.
+ */
+public class DataFormatException extends Exception {
+    public DataFormatException(String msg)
+    {
+        super(msg);
+    }
+}
---
+++ src/Pair.java
@@ -0,0 +1,28 @@
+/**
+ * Created by lenovo on 3/31/2015.
+ */
+public class Pair{
+    private float x;
+    private float y;
+
+    public Pair(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
